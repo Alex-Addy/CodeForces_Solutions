2aab809b5a8e9bf803e04b17a729130d83b18e76
Alex Addy <alex.addy@gmail.com>
2014-02-23 13:02:51 -0500

    Simplified program by using some pattern matching.

--- 131A-cAPS_lOCK.hs
+++ 131A-cAPS_lOCK.hs
@@ -9,6 +9,6 @@ main = do
     putStrLn $ fixCaps word
 
 fixCaps :: String -> String
-fixCaps s = if' (foldl (\x y -> x && (isUpper y)) True s) (map toLower s) $
-            if' (foldl (\x y -> x && (isUpper y)) (isLower $ head s) (tail s)) ([toUpper $ head s] ++ (map toLower $ tail s)) $
-            s
+fixCaps s@(x:xs) = if' (foldl (\x y -> x && (isUpper y)) True s) (map toLower s) $
+            		if' (foldl (\x y -> x && (isUpper y)) (isLower $ x) (xs)) ([toUpper $ x] ++ (map toLower $ xs)) $
+            		s
